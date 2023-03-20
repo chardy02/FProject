6bb3c4f003bf90d0d2b4f11fd21119338647dcf8
PropaaPanda <cameronhardy26@gmail.com>
2023-03-20 14:09:10 -0700

    Changed line object construction/drawing

--- Line.java
+++ Line.java
@@ -1,25 +1,26 @@
 import java.awt.*;
 
 public class Line implements Drawable {
-    private CodeBlock start;
-    private CodeBlock end;
+    private Point start;
+    private Point end;
 
     public Line(CodeBlock s, CodeBlock e) {
-        start = s;
-        end = e;
+        Shape sShape = (Shape) start.getShape();
+        Shape eShape = (Shape) end.getShape();
+        start = new Point(sShape.getCenterX(), sShape.getCenterY());
+        end = new Point(eShape.getCenterX(), eShape.getCenterY());
+    }
+    public Line(Point s, Point e) {
+
     }
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        Shape sShape = (Shape) start.getShape();
-        Shape eShape = (Shape) end.getShape();
 
-        g2d.drawLine(sShape.getCenterX(), sShape.getCenterY(), eShape.getCenterX(), eShape.getCenterY());
+        g2d.drawLine(start.x, start.y, end.x, end.y);
     }
 
     @Override
-    public int getXCenter() {
-        return 0;
-    }
+    public int getXCenter() { return 0; }
     @Override
     public int getYCenter() {
         return 0;
