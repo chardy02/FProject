8ffaf722499446450ac969c9b27e2a4663964b94
aaron <aabettschool@outlook.com>
2023-03-20 10:59:34 -0700

    Renaamed Instruction to InstructionBlock, Loop to LoopBlock

---
+++ InstructionBlock.java
@@ -0,0 +1,15 @@
+public class InstructionBlock extends CodeBlock {
+
+    public InstructionBlock(Drawable wrapper, int maxInboundCount, int maxOutboundCount) {
+        super(wrapper,1,1);
+    }
+
+    public void setXCenter(int x) {
+        setXCenter(x);
+    }
+
+    public void setYCenter(int y) {
+        setYCenter(y);
+    }
+
+}
--- Loop.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-public class Loop extends CodeBlock {
-
-    public Loop(Drawable wrapper) {
-        super(wrapper, 0, 4);
-    }
-
-}
