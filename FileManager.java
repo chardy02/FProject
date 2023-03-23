1623bf29b7d108a3358032066b4f851e31f50da0
PropaaPanda <cameronhardy26@gmail.com>
2023-03-22 17:41:38 -0700

    Made final changes and added JavaDoc and comments

--- FileManager.java
+++ FileManager.java
@@ -1,35 +1,49 @@
 import java.io.*;
 import java.util.*;
-
+/**
+ * @author Cameron Hardy
+ * This class is responsible for saving and loading diagrams to/from file
+ */
 public class FileManager {
+    /**
+     * Reads the diagram with the given filename
+     * First reads the file to construct the CodeBlocks present in the diagram
+     * Then adds each line to the Repository from each outboundcodeblock reference list
+     * @param filename - String representing filename to read from
+     */
     public static void readFile(String filename) {
+        // List to store read in blocks
         ArrayList<CodeBlock> blocks = new ArrayList<>();
-        ArrayList<Line> lines = new ArrayList<>();
         try {
+            // Create an input stream, read in the file, casting to ArrayList
             FileInputStream fis = new FileInputStream(filename + ".diag");
             ObjectInputStream ois = new ObjectInputStream(fis);
             blocks = (ArrayList<CodeBlock>) ois.readObject();
             ois.close();
+            // For each CodeBlock, add line to outbound CodeBlocks
+            for(CodeBlock b : blocks) {
+                for(CodeBlock out : b.getOutboundCodeBlocks()) {
+                    Repository.getInstance().addLine(new Line(b, out));
+                }
+            }
+            Repository.getInstance().setCodeBlocks(blocks);
         } catch (IOException | ClassNotFoundException e) {
+            // Print Error if file not read
+            e.printStackTrace();
             System.out.println("Error reading file: " + e.getMessage());
         }
-        for(CodeBlock b : blocks) {
-            for(CodeBlock out : b.getOutboundCodeBlocks()) {
-                Repository.getInstance().addLine(new Line(b, out));
-            }
-        }
-        Repository.getInstance().setCodeBlocks(blocks);
     }
 
     public static void writeFile(String filename) {
         try {
+            // Open output stream, write the CodeBlocks from Repository
             FileOutputStream fos = new FileOutputStream(filename + ".diag");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-
             oos.writeObject(Repository.getInstance().getCodeBlocks());
             oos.flush();
             oos.close();
         } catch (IOException e) {
+            // Print error if it could not write to file
             e.printStackTrace();
             System.out.println("Error writing to file: " + e.getMessage());
         }
--- Line.java
+++ Line.java
@@ -1,49 +1,59 @@
 import java.awt.*;
 
-
+/**
+ * Line class responsible for drawing connections between two CodeBlocks, with directionality
+ * @author Cameron Hardy
+ */
 public class Line implements Drawable {
+    /**
+     * @Field start - CodeBlock being drawn from
+     * @Field end - CodeBlock being drawn to
+     */
     private CodeBlock start;
     private CodeBlock end;
 
+    /**
+     * @Field arrowLen - Defines the length of the arrow head lines
+     */
+    private int arrowLen = 15;
+
+    /**
+     * Assigns starting and ending CodeBlocks
+     * Add CodeBlocks to inbound and outbound respectively
+     * @param s - Starting CodeBlock
+     * @param e - Ending CodeBlock
+     */
     public Line(CodeBlock s, CodeBlock e) {
         start = s;
         end = e;
         s.addToOutbound(e);
         e.addToInbound(s);
-//        Drawable sShape = s.getShape();
-//        Drawable eShape = e.getShape();
-//        start = new Point(sShape.getXCenter(), sShape.getYCenter());
-//        end = new Point(eShape.getXCenter(), eShape.getYCenter());
     }
-//    public Line(Point s, Point e) {
-//        start = s;
-//        end = e;
-//    }
+
+    /**
+     * Draws the line, and arrow head
+     * @param g - Graphics object used for drawing
+     */
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
+        // Sets the start and end positions
         Point startPos = new Point(start.getXCenter(), start.getYCenter() + (start.getHeight() / 2));
         Point endPos = new Point(end.getXCenter(), end.getYCenter() - (end.getHeight() / 2));
 
-        if(startPos.x < endPos.x) {
-            if(startPos.y < endPos.y) {
-                //negative deg
-            }
-//        determine slope of line to calculate angle...
-        }
+        // Calculates the angle the line makes
+        double theta = Math.atan2((double) (endPos.y - startPos.y), (double)(endPos.x - startPos.x));
+        // Calculate the points for arrow head drawing
+        Point ahead1 = new Point(endPos.x - (int) (arrowLen * Math.cos(theta - Math.toRadians(45))),
+                endPos.y - (int) (arrowLen * Math.sin(theta - Math.toRadians(45))));
+        Point ahead2 = new Point(endPos.x - (int) (arrowLen * Math.cos(theta + Math.toRadians(45))),
+                endPos.y - (int) (arrowLen * Math.sin(theta + Math.toRadians(45))));
 
+        // Set the color, stroke, and draw line and arrow head
         g.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(2));
         g2d.drawLine(startPos.x, startPos.y, endPos.x, endPos.y);
-//        double theta = Math.atan((double) (endPos.y - startPos.y) / (double)(endPos.x - startPos.x));
-//        System.out.println(theta * 360 / (2 * Math.PI));
+        g2d.drawLine(endPos.x, endPos.y, ahead1.x, ahead1.y);
+        g2d.drawLine(endPos.x, endPos.y, ahead2.x, ahead2.y);
     }
-
-//    public void update(Point newS, Point newE) {
-//        start = newS;
-//        end = newE;
-//    }
-//    public void follow(Point endPt) {
-//        end = endPt;
-//    }
-
 }
