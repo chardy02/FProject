4cbdfd3027c1142ce2b7de07b1158572b75f240e
connor-addison-h <connorahickey@gmail.com>
2023-03-22 17:44:43 -0700

    Instruction and Loop Block Comments

--- InstructionBlock.java
+++ InstructionBlock.java
@@ -1,5 +1,14 @@
+
+/**
+ * Represents an InstructionBlock which extends CodeBlock.
+ * @author Connor Hickey
+ */
 public class InstructionBlock extends CodeBlock {
 
+    /**
+     * Constructs a new InstructionBlock with the given shape (wrapper).
+     * @param wrapper The Shape of the InstructionBlock.
+     */
     public InstructionBlock(Shape wrapper) {
         super(wrapper,1,1);
     }
--- LoopBlock.java
+++ LoopBlock.java
@@ -1,6 +1,14 @@
-public class LoopBlock extends CodeBlock {
 
+/**
+ * Represents a LoopBlock, which is a specialized type of CodeBlock for loops.
+ * @author Connor Hickey
+ */
+ public class LoopBlock extends CodeBlock {
 
+    /**
+     * Constructs a new LoopBlock with the given shape (drawable).
+     * @param drawable The Shape of the LoopBlock.
+     */
     public LoopBlock(Shape drawable) {
         super(drawable, 2, 2);
     }
