28651e33a13a3615b16d22bd83d0a3719012c88c
connor-addison-h <connorahickey@gmail.com>
2023-03-22 17:47:45 -0700

    PerpendicularLineDecorator java docs

--- PerpendicularLineDecorator.java
+++ PerpendicularLineDecorator.java
@@ -1,10 +1,23 @@
 import java.awt.*;
 
+/**
+ * Represents a PerpendicularLineDecorator that decorates a Shape with perpendicular lines.
+ * @author Connor Hickey
+ */
 public class PerpendicularLineDecorator extends ShapeDecorator {
+
+    /**
+     * Constructs a new PerpendicularLineDecorator with the given inner shape.
+     * @param innerShape The Shape to be decorated.
+     */
     public PerpendicularLineDecorator(Shape innerShape) {
         super(innerShape);
     }
 
+    /**
+     * Draws the decorated Shape along with perpendicular lines.
+     * @param g The Graphics object to draw with.
+     */
     @Override
     public void draw(Graphics g) {
         super.draw(g);
